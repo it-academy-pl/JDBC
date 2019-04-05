a371418edd17950ad232912cb02d24fa21ff6732
seredao <olegik.poltava@gmail.com>
2019-04-05 23:12:05 +0200

    init commit for JDBC project

---
+++ src/main/java/pl/itacademy/repository/ConnectionManager.java
@@ -0,0 +1,11 @@
+package pl.itacademy.repository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    public Connection getConnection() throws SQLException {
+        return null;
+    }
+}
